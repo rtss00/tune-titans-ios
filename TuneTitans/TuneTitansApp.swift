b86f0dbadedaed43da3fd10efb076ea39252d52e
Renzo Tissoni <r.tissoni@gmail.com>
2023-03-23 09:09:04 -0300

    Initial Commit

---
+++ TuneTitans/TuneTitansApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneTitansApp.swift
+//  TuneTitans
+//
+//  Created by Renzo Tissoni on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TuneTitansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
